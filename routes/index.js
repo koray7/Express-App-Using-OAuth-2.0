8780b2f23ddd34413da3588654f5423a79e81f18
Koray7 <softwarekoray@gmail.com>
2023-08-17 14:14:34 -0500

    Login and Logout created using OAuth-2.0

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,11 @@ var router = express.Router();
 
 router.get("/", (req, res) => {
   console.log(req.oidc.isAuthenticated());
-  res.render("index", { title: "Express Demo" });
+  res.render("index", {
+    title: "Express Demo",
+    isAuthenticated: req.oidc.isAuthenticated(),
+    user: req.oidc.user,
+  });
 });
 
 module.exports = router;
--- server.js
+++ server.js
@@ -23,12 +23,12 @@ app.get("/", function (req, res) {
   // });
 });
 
-app.get("/api/public", function (req, res) {
-  res.json({
-    message:
-      "Hello from a public endpoint! You don't need to be authenticated to see this.",
-  });
-});
+// app.get("/api/public", function (req, res) {
+//   res.json({
+//     message:
+//       "Hello from a public endpoint! You don't need to be authenticated to see this.",
+//   });
+// });
 
 app.get("/api/private", checkJwt, function (req, res) {
   res.json({
