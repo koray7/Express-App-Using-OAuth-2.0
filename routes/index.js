2a416cda8e8a26da3b7c4b3d01fe8391698ff875
Koray7 <softwarekoray@gmail.com>
2023-08-18 15:47:58 -0500

    API endpoint created on different App and calling that data on my App using axios (TEST)

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,7 @@
 var express = require("express");
 var router = express.Router();
+const { requiresAuth } = require("express-openid-connect");
+const axios = require("axios");
 
 router.get("/", (req, res) => {
   console.log(req.oidc.isAuthenticated());
@@ -8,6 +10,24 @@ router.get("/", (req, res) => {
     title: "Welcome to my page!",
     isAuthenticated: req.oidc.isAuthenticated(),
     user: req.oidc.user,
+  });
+});
+
+router.get("/secured", requiresAuth(), async (req, res) => {
+  let data = {};
+
+  try {
+    const apiResponse = await axios.get("http://localhost:8000/public");
+    data = apiResponse.data;
+  } catch (err) {}
+
+  console.log(req.oidc.isAuthenticated());
+  res.render("Secured", {
+    name: "Secured Page",
+    title: "Welcome to my page!",
+    isAuthenticated: req.oidc.isAuthenticated(),
+    user: req.oidc.user,
+    data,
   });
 });
 
